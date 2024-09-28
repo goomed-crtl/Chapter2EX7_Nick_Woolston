acf8701062506f246582c0b5214720c9d45d4106
goomed-crtl <alohanickw@hotmail.com>
2024-09-27 21:15:07 -0400

    Add project files.

---
+++ Chapter2EX7_Nick_Woolston/Chapter2EX7_Nick_Woolston.cpp
@@ -0,0 +1,32 @@
+﻿/* Title: Chapter 2 Exerise 7 - Ocean Levels
+File Name : Chapter2EX7_Nick_Woolston.cpp
+Programmer : Nicholas Woolston
+Date : 27 / 09 / 2024
+Requirements:
+Assuming the ocean’s level is currently rising at about 1.5 millimeters per year, write a program that displays:
+
+• The number of millimeters higher than the current level that the ocean’s level will be in 5 years.
+
+• The number of millimeters higher than the current level that the ocean’s level will be in 7 years.
+
+• The number of millimeters higher than the current level that the ocean’s level will be in 10 years.
+
+Once you have coded this and placed in your main branch in GitHub, create a second branch that revises your code so
+
+that the user can enter the number of the number of millimeters the ocean level rises per year..
+*/
+
+#include <iostream>
+using namespace std;
+const double oceanRise = 1.5;
+const double fiveYears = 5;
+const double sevenYears = 7;
+const double tenYears = 10;
+
+int main()
+{
+    cout << " The ocean will rise " << oceanRise * fiveYears << " meters in five years, " << oceanRise * sevenYears << " meters in seven years, and " << oceanRise * tenYears << " meters in ten years.";
+    return 0;
+}
+
+
